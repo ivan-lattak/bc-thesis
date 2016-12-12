31b59b351437e024cfb6877893e38133eb03eaf4
Ivan Latták <ivanlattak@gmail.com>
2016-12-12 19:49:20 +0100

    Temporarily disable compiling

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PATH=$PATH:/usr/bin
+
 cleanup() {
 	killall sleep
 	# make -s clean
@@ -9,8 +11,8 @@ cleanup() {
 [ -d include ] || mkdir include
 cat solution/sol1 from_browser solution/sol2 > include/solution.h
 
-make -s -j4 || exit
+#make -s -j4 || exit
 
 (./main ; cleanup) & pid=$!
-(sleep 10 && kill $pid 2>/dev/null && echo TLE)
+(sleep 10 && kill $pid && echo TLE)
 

4c0678681da9a176ca457ee652eb460fdcabb13a
Ivan Latták <ivanlattak@gmail.com>
2016-12-12 15:08:46 +0100

    TLE now kills main correctly

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+cleanup() {
+	killall sleep
+	# make -s clean
+	exit
+}
+
 [ -d include ] || mkdir include
 cat solution/sol1 from_browser solution/sol2 > include/solution.h
 
-make -s -j 4 || exit
+make -s -j4 || exit
 
-./main
+(./main ; cleanup) & pid=$!
+(sleep 10 && kill $pid 2>/dev/null && echo TLE)
 
-# make -s clean

1f8958801ff64ef573cdd62c2cebb3278887c1f8
Ivan Lattak <ivanlattak@gmail.com>
2017-01-20 21:05:15 +0100

    Move admin redirect functionality to refactoring/urls

--- thesis/urls.py
+++ thesis/urls.py
@@ -14,9 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
 
 urlpatterns = [
     url(r'^refactoring/', include('refactoring.urls')),
-    url(r'^refactoring/admin/', admin.site.urls),
 ]

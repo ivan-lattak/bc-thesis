59f496b6f7c0dc92fe4cc7a808186c42752c3af9
Ivan Lattak <ivanlattak@gmail.com>
2017-01-20 20:46:17 +0100

    Revert creating new urls, rename css file

--- refactoring/urls.py
+++ refactoring/urls.py
@@ -1,15 +1,7 @@
-from django.conf.urls import include, url
-from django.views.generic.edit import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url('^register/', CreateView.as_view(
-            template_name='register.html',
-            form_class=UserCreationForm,
-            success_url='/'
-    )),
-    url('^accounts/', include('django.contrib.auth.urls')),
 ]

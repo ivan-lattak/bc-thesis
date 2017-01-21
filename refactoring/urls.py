ee8a29e709468ee7a0849332cd6003000368d337
Ivan Lattak <ivanlattak@gmail.com>
2017-01-21 17:40:32 +0100

    Realign `refactoring/urls.py`

--- refactoring/urls.py
+++ refactoring/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/',    admin.site.urls                ),
+    url(r'^$',         views.index,    name='index'   ),
     url(r'^register/', views.register, name='register'),
 ]

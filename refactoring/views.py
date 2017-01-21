540824bac319aa920c510a840dc0581edac2cadd
Ivan Lattak <ivanlattak@gmail.com>
2017-01-21 17:17:28 +0100

    Factor out LOGIN_REDIRECT_URL to settings

--- refactoring/views.py
+++ refactoring/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.conf import settings
 from django.contrib.auth import login
+from django.contrib.auth.models import User
 
 from .forms import RefactoringForm, UserForm
 
@@ -34,7 +36,8 @@ def register(request):
         form = UserForm(request.POST)
         if form.is_valid():
             new_user = User.objects.create_user(**form.cleaned_data)
-            login(new_user)
-            return HttpResponseRedirect('refactoring/')
+            new_user.save()
+            login(request, new_user)
+            return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
 
     return render(request, 'refactoring/register.html', {'form': form})

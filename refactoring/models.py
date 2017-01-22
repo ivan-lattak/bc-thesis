26bc8426ef5eb66589dd83ade003799e3b99f060
Ivan Lattak <ivanlattak@gmail.com>
2017-01-22 17:00:31 +0100

    Create models for exercises and test cases

--- refactoring/models.py
+++ refactoring/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import re
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Exercise(models.Model):
+    exercise_text = models.TextField()
+    original_code = models.TextField()
+    solved_by = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return "Exercise {}".format(self.id)
+
+
+class TestCase(models.Model):
+    code = models.TextField()
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+
+    matcher = re.compile(r'^TEST\([^\,\)]*\,[^\)]*\)')
+
+    def __str__(self):
+        case_name = TestCase.matcher.match(self.code).group(0)
+        return "TestCase {} of exercise {}".format(case_name, self.exercise.id)

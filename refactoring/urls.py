8680594c4a2ade995fc3aa8febc02295b6de9bec
Ivan Lattak <ivanlattak@gmail.com>
2017-03-30 22:53:21 +0200

    Add $ to end of url matching patterns

--- refactoring/urls.py
+++ refactoring/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^admin/',                                                         admin.site.urls                            ),
     url(r'^auth/',                                                          include('django.contrib.auth.urls')        ),
-    url(r'^auth/register/',                                                 views.register, name='register'            ),
-    url(r'^auth/logged_out/',                                               views.logged_out, name='logged_out'        ),
+    url(r'^auth/register/$',                                                views.register, name='register'            ),
+    url(r'^auth/logged_out/$',                                              views.logged_out, name='logged_out'        ),
     url(r'^(?P<exercise_id>[0-9]+)/$',                                      views.detail, name='detail'                ),
     url(r'^(?P<exercise_id>[0-9]+)/sessions/$',                             views.sessions, name='sessions'            ),
     url(r'^(?P<exercise_id>[0-9]+)/solutions/$',                            views.solutions, name='solutions'          ),
